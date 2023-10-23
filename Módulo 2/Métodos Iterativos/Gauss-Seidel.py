9a64f7c4902a2176d4cee3acc6fa6a62b318ceea
leodavid0109 <leodavid0521@gmail.com>
2023-10-23 08:40:05 -0500

    Completo Métodos Iterativos

--- Módulo 2/Métodos Iterativos/Gauss-Seidel.py
+++ Módulo 2/Métodos Iterativos/Gauss-Seidel.py
@@ -1,1 +1,53 @@
-# David
+def gaussSeidel(n, matrix, resultant, beginning, iterations):
+    for k in range(iterations):
+        for i in range(n):
+            beginning[i] = (resultant[i] - sum((matrix[i][j] * beginning[j]) for j in range(n) if i != j)) / matrix[i][i]
+    return beginning
+
+if __name__ == "__main__":
+    # Caso de Prueba 1. Tomado del ejemplo 3 del libro
+    A = [[2, -1, 0],
+         [1, 6, -2],
+         [4, -3, 8]]
+    x = [0, 0, 0]
+    b = [2, -4, 5]
+
+    result = gaussSeidel(3, A, b, x, 20)
+    print("Vector resultante: " + str(result))
+    conf = [0] * 3
+    for i in range(3):
+        for j in range(3):
+            conf[i] += (A[i][j] * result[j])
+    print("Ax=b: " + str(conf))
+    print()
+
+    # Caso de Prueba 2
+    A = [[4, 1, 2],
+         [3, 5, 2],
+         [1, 2, 5]]
+    x = [1, 1, 1]
+    b = [8, 9, 9]
+
+    result = gaussSeidel(3, A, b, x, 20)
+    print("Vector resultante: " + str(result))
+    conf = [0] * 3
+    for i in range(3):
+        for j in range(3):
+            conf[i] += (A[i][j] * result[j])
+    print("Ax=b: " + str(conf))
+    print()
+
+    # Caso de Prueba 3
+    A = [[3, 1, 1],
+         [2, 4, 1],
+         [1, 1, 3]]
+    x = [0, 0, 0]
+    b = [5, 7, 4]
+
+    result = gaussSeidel(3, A, b, x, 20)
+    print("Vector resultante: " + str(result))
+    conf = [0] * 3
+    for i in range(3):
+        for j in range(3):
+            conf[i] += (A[i][j] * result[j])
+    print("Ax=b: " + str(conf))
--- Módulo 2/Métodos Iterativos/Jacobi.py
+++ Módulo 2/Métodos Iterativos/Jacobi.py
@@ -6,3 +6,51 @@ def jacobi(n, matrix, resultant, beginning, iterations):
         for i in range(n):
             beginning[i] = auxiliar[i]
     return beginning
+
+if __name__ == "__main__":
+    # Caso de Prueba 1
+    A = [[4, -1, 0],
+         [-1, 4, -1],
+         [0, -1, 3]]
+    x = [0, 0, 0]
+    b = [7, 5, 4]
+
+    result = jacobi(3, A, b, x, 20)
+    print("Vector resultante: " + str(result))
+    conf = [0] * 3
+    for i in range(3):
+        for j in range(3):
+            conf[i] += (A[i][j] * result[j])
+    print("Ax=b: " + str(conf))
+    print()
+
+    # Caso de Prueba 2
+    A = [[4, 1, 2],
+         [3, 5, 2],
+         [1, 2, 5]]
+    x = [1, 1, 1]
+    b = [8, 9, 9]
+
+    result = jacobi(3, A, b, x, 20)
+    print("Vector resultante: " + str(result))
+    conf = [0] * 3
+    for i in range(3):
+        for j in range(3):
+            conf[i] += (A[i][j] * result[j])
+    print("Ax=b: " + str(conf))
+    print()
+
+    # Caso de Prueba 3
+    A = [[3, 1, 1],
+         [2, 4, 1],
+         [1, 1, 3]]
+    x = [0, 0, 0]
+    b = [5, 7, 4]
+
+    result = jacobi(3, A, b, x, 20)
+    print("Vector resultante: " + str(result))
+    conf = [0] * 3
+    for i in range(3):
+        for j in range(3):
+            conf[i] += (A[i][j] * result[j])
+    print("Ax=b: " + str(conf))
